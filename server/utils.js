74c74ec3b93ef0ce1aa4eca8606e75080c451bd1
Jake Peyser <jakepeyser@gmail.com>
2017-01-17 17:51:45 -0500

    implemented search API routes

---
+++ server/utils.js
@@ -0,0 +1,55 @@
+const request = require('request');
+
+// GitHub API call wrapper
+const githubRequest = (req, res, next, url, success) => {
+  const options = {
+    url,
+    headers: {
+      'User-Agent': 'git-lucky',
+      'Authorization': `token ${req.githubAuthToken}`
+    }
+  };
+  request(options, (error, response, body) => {
+    if (error) {
+      return next(error)
+    }
+
+    // Send back result of success callback
+    if (response.statusCode === 200) {
+      const resultPayload = success(body);
+      res.send(resultPayload);
+    } else {
+      // Bad token or rate limit exceeded errors
+      return next({
+        status: response.statusCode,
+        message: JSON.parse(body).message || ''
+      })
+    }
+  });
+}
+
+// Extract core repo details from the verpose GitHub API response
+const getRepoDetails = (repo) => ({
+  id: repo.id,
+  name: repo.name,
+  description: repo.description,
+  language: repo.language,
+  private: repo.private,
+  createdAt: repo.created_at,
+  updatedAt: repo.updated_at,
+  links: {
+    htmlUrl: repo.html_url,
+    homepage: repo.homepage
+  },
+  stats: {
+    stars: repo.stargazers_count,
+    watchers: repo.watchers_count,
+    forks: repo.forks_count
+  }
+});
+
+const processSearchResult = (result) => {
+  return JSON.parse(result).items.map(getRepoDetails);
+}
+
+module.exports = { githubRequest, processSearchResult };
